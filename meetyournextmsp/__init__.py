80dda5233c3fa8437686dc39fe0a656302576558
James <james.baster@opendataservices.coop>
2021-03-20 14:30:52 +0000

    Event page - basic start

--- meetyournextmsp/__init__.py
+++ meetyournextmsp/__init__.py
@@ -1,6 +1,8 @@
 import os
+import sqlite3
 
-from flask import Flask, render_template
+
+from flask import Flask, render_template, abort
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -18,7 +20,8 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
+
+
     @app.route("/")
     def index():
         return render_template('index.html')
@@ -26,5 +29,16 @@ def create_app(test_config=None):
     @app.route("/contribute")
     def contribute():
         return render_template('contribute.html')
+
+    @app.route("/event/<id>")
+    def event(id):
+        database = sqlite3.connect(app.config['DATABASE'])
+        database.row_factory = sqlite3.Row
+        cur = database.cursor()
+        cur.execute('SELECT * FROM event WHERE id=?', [id])
+        event = cur.fetchone()
+        if not event:
+            abort(404)
+        return render_template('event.html', event=event)
 
     return app
